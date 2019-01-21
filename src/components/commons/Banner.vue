a598358ea2d6dfea8087d69948195937d45e0771
ming <840061073@qq.com>
2019-01-21 23:34:51 +0800

    ‘m轮播图组件

---
+++ src/components/commons/Banner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <mt-swipe :auto="auto" :speed="speed" class="banner">
+      <mt-swipe-item class="banner-item" v-for= "(item,idx) in imgurl" :key="idx">
+        <img class="img" :src="item" alt>
+      </mt-swipe-item>
+      <!-- <mt-swipe-item>32222</mt-swipe-item> -->
+    </mt-swipe>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      auto: 5000,//自动播放间隔时长
+      speed: 300,//动画持续时长
+      imgurl:[
+          "https://pic.keede.com/Images/2019app/20190118/banner_sb.jpg",
+          "https://pic.keede.com/Images/2018app/20181214/banner_sj.jpg",
+          "https://pic.keede.com/event/2019/2018nhjqs/banner_sj.jpg",
+          "https://pic.keede.com/event/2019/2019han/banner-kj.jpg",
+          "https://pic.keede.com/event/2019/2019han/banner-kj.jpg"
+      ]
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+@import url("../../styls/main.less");
+div {
+  .banner {
+    .h(225);
+    .banner-item {
+      .h(225);
+      .img {
+        .w(375);
+        .h(245);
+      }
+    }
+  }
+}
+</style>
--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,37 +1,24 @@
 <template>
   <div class="home">
-    <!-- <h1>{{name}}</h1> -->
-    首页
-    <!-- <Search></Search> -->
-    <mt-swipe :auto="4000" class="banner">
-      <mt-swipe-item>12222</mt-swipe-item>
-      <mt-swipe-item>22222</mt-swipe-item>
-      <mt-swipe-item>32222</mt-swipe-item>
-    </mt-swipe>
+    <Search></Search>
+    <Banner></Banner>
+    
   </div>
+  
 </template>
 
 
 <script type="text/javascript">
 // 引入banner和homelist组件
-import Home from "./Home.vue";
+import Banner from "../../commons/Banner.vue";
 import Search from "../../commons/Search.vue";
-// import { Swipe, SwipeItem } from "mint-ui";
-// Vue.component(Swipe.name, Swipe);
-// Vue.component(SwipeItem.name, SwipeItem);
 export default {
   name: "Home",
   // 注册组件
-  components: { Home, Search },
+  components: { Search,Banner },
   data() {
     return {
-      name: "这里是home组件",
-      test: "您好",
-      movie_type: [
-        { title: "正在热映", path: "now-playing" },
-        { title: "即将上映", path: "coming-soon" }
-      ]
-    };
+    }
   }
 
   // computed:{
@@ -47,7 +34,5 @@ export default {
 
 <style lang="less" scoped>
 @import url("../../../styls/main.less");
-.banner {
-  height: 100px;
-}
+
 </style>
